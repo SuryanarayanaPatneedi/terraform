ba9ffa397c6c3cb7ffff7317b2b7cc2fde7d99fd
Surya <suryapatneedi0505@gmail.com>
2021-07-31 23:20:19 +0530

    nothing

---
+++ roboshop-shell-scripting/ec2.tf
@@ -0,0 +1,52 @@
+resource "aws_spot_instance_request" "cheap_worker" {
+  count                     = local.LENGTH
+  ami                       = "ami-074df373d6bafa625"
+  spot_price                = "0.0031"
+  instance_type             = "t3.micro"
+  vpc_security_group_ids    = ["sg-0fdabf3f5a8512ce2"]
+  wait_for_fulfillment      = true
+  //spot_type                 = "persistent"
+  tags                      = {
+    Name                    = element(var.COMPONENTS, count.index)
+  }
+}
+
+resource "aws_ec2_tag" "name-tag" {
+  count                     = local.LENGTH
+  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key                       = "Name"
+  value                     = element(var.COMPONENTS, count.index)
+}
+
+resource "aws_route53_record" "records" {
+  count                     = local.LENGTH
+  name                      = element(var.COMPONENTS, count.index)
+  type                      = "A"
+  zone_id                   = "Z1029136290Z1TNWYA0ZF"
+  ttl                       = 300
+  records                   = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
+}
+
+resource "null_resource" "run-shell-scripting" {
+  depends_on                = [aws_route53_record.records]
+  count                     = local.LENGTH
+  provisioner "remote-exec" {
+    connection {
+      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
+      user                  = "centos"
+      password              = "DevOps321"
+    }
+
+    inline = [
+      "cd /home/centos",
+      "git clone https:/https://github.com/SuryanarayanaPatneedi/Shell-scripting.git",
+      "cd terraform ",
+      "cd shell-scripting/roboshop",
+      "sudo make ${element(var.COMPONENTS, count.index)}"
+    ]
+  }
+}
+
+locals {
+  LENGTH    = length(var.COMPONENTS)
+}
---
+++ roboshop-shell-scripting/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket            = "surya-terraform"
+    key               = "roboshop/shell-scripting/terraform.tfstate"
+    region            = "us-east-1"
+    dynamodb_table    = "terraform"
+  }
+}
