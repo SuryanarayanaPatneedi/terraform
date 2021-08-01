c09ebea791979ab39c7f8fe1d330ed36f304f8fb
Surya <suryapatneedi0505@gmail.com>
2021-08-01 17:47:24 +0530

    nothing

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -22,7 +22,7 @@ resource "aws_route53_record" "records" {
   count                     = local.LENGTH
   name                      = element(var.COMPONENTS, count.index)
   type                      = "A"
-  zone_id                   = "Z1029136290Z1TNWYA0ZF"
+  zone_id                   = "Z029597183QHVTB3PO8K"
   ttl                       = 300
   records                   = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
 }
